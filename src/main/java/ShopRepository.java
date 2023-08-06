6e08d8f8079a6a632b60d70385d123cbc7f15b43
Anna Russkikh <nyusha82@inbox.ru>
2023-08-06 16:27:38 +0300

    Change commit

---
+++ src/main/java/ShopRepository.java
@@ -0,0 +1,79 @@
+public class ShopRepository {
+    private Product[] products = new Product[0];
+    private ShopRepository repo;
+
+    /**
+     * Метод добавления товара в репозиторий
+     * @param product — добавляемый товар
+     */
+    public void add(Product product) {
+        products = addToArray(products, product);
+
+//        Product addingProduct = findByExistsId(product.getId());
+//         if (addingProduct != null) {
+//            throw new AlreadyExistsException(
+//                    "Товар с id: "+ product.getId() +" уже существует"
+//            );
+//        }
+    }
+
+    /**
+     * Вспомогательный метод для имитации добавления элемента в массив
+     * @param current — массив, в который мы хотим добавить элемент
+     * @param product — элемент, который мы хотим добавить
+     * @return — возвращает новый массив, который выглядит, как тот, что мы передали,
+     * но с добавлением нового элемента в конец
+     */
+    private Product[] addToArray(Product[] current, Product product) {  // Создаем массив на единицу длиннее
+        Product[] tmp = new Product[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = product;
+        return tmp;
+    }
+//    private Product findByExistsId(int id) {
+//        for (Product product : products) {
+//            if (product.getId() == id) {
+//                return product;
+//            }
+//        }
+//        return null;
+//    }
+//    public Product[] getProducts() {
+//        return products;
+//    }
+    public Product[] findAll() {
+        return products;
+    }
+
+    // Этот способ мы рассматривали в теории в теме про композицию
+    public void remove(int id) {
+
+        Product removingProduct = findById(id);
+        if (removingProduct == null) {
+            throw new NotFoundException(
+                    "Товар с id: " + id + " не найден"
+            );
+        }
+        
+        Product[] tmp = new Product[products.length - 1];    // Создаем массив на единицу короче
+        int copyToIndex = 0;                              // Переменная, отвечающая за индекс
+        for (Product product : products) {
+            if (product.getId() != id) {         // Перебираем все продукты
+                tmp[copyToIndex] = product;      // Если id не совпадает с исключаемым, то товар копируется
+                copyToIndex++;                   // индекс увеличится
+            }                                    // Если id совпадает с исключаемым, то ничего не происходит
+        }
+        products = tmp;
+    }
+
+    private Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id){
+                return product;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,105 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    @Test
+    void removeWhenProductExist() {
+
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "книга", 2_000);
+        Product product2 = new Product(22, "футболка", 3_000);
+        Product product3 = new Product(333, "кружка", 1_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(22);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+//    @Test
+//    void removeWhenProductNotExist() {
+//
+//        ShopRepository repo = new ShopRepository();
+//
+//        Product product1 = new Product(1, "книга", 2_000);
+//        Product product2 = new Product(22, "футболка", 3_000);
+//        Product product3 = new Product(333, "кружка", 1_000);
+//
+//        repo.add(product1);
+//        repo.add(product2);
+//        repo.add(product3);
+//        repo.remove(4444);
+//
+//        Product[] actual = repo.findAll();
+//        Product[] expected = {product1, product2, product3};
+//
+//        Assertions.assertArrayEquals(expected, actual);
+//    }
+    @Test
+    void testRemoveWhenProductNotExist() {
+
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "книга", 2_000);
+        Product product2 = new Product(22, "футболка", 3_000);
+        Product product3 = new Product(333, "кружка", 1_000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () ->
+            repo.remove(4444)
+        );
+    }
+//    @Test
+//    void addWhenProductNotExist() {
+//
+//        ShopRepository repo = new ShopRepository();
+//
+//        Product product1 = new Product(1, "книга", 2_000);
+//        Product product2 = new Product(22, "футболка", 3_000);
+//        Product product3 = new Product(333, "кружка", 1_000);
+//        Product product4 = new Product(4444,"машинка",10_000);
+//
+//        repo.add(product1);
+//        repo.add(product2);
+//        repo.add(product3);
+//        repo.add(product4);
+//
+//        Product[] actual = repo.findAll();
+//        Product[] expected = {product1, product2, product3, product4};
+//
+//        Assertions.assertArrayEquals(expected, actual);
+//
+//    }
+//    @Test
+//    void addWhenProductExist() {
+//
+//        ShopRepository repo = new ShopRepository();
+//
+//        Product product1 = new Product(1, "книга", 2_000);
+//        Product product2 = new Product(22, "футболка", 3_000);
+//        Product product3 = new Product(333, "кружка", 1_000);
+//        Product product4 = new Product(1,"книга",2_000);
+//
+//        repo.add(product1);
+//        repo.add(product2);
+//        repo.add(product3);
+//        repo.add(product4);
+//
+//        Assertions.assertThrows(AlreadyExistsException.class, () ->
+//                repo.add(product4)
+//        );
+//
+//    }
+}
